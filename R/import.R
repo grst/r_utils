5791081b250650fe1b724bafe40b33d0cf7db611
Gregor Sturm <mail@gregor-sturm.de>
2018-05-07 15:13:05 +0000

    add stub functions

---
+++ R/import.R
@@ -0,0 +1,14 @@
+#' import a package silently
+import = function(pkg) {
+
+}
+
+#' import a specific function from a package
+#' use a named vector to change the name of objects
+import_from = function(package, objs) {
+
+}
+
+
+
+
---
+++ R/io.R
@@ -0,0 +1,4 @@
+#' read data from an R object and store it into a variable.
+read_rdata = function(rdata_file, obj=NULL) {
+
+}
